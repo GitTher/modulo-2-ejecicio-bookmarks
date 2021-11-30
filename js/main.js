9d55b174a8280082be8b30b0e4d96402d6792a57
Inma Sánchez Casabán <iscasaban@gmail.com>
2021-11-30 11:27:36 +0100

    exercise 1&2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@
 
 /* Dia 1*/
 /* 1- Prueba mostrar formulario nuevo bookmark */
+
+let addBookmark = document.querySelector (".data-actions__add");
+
+addBookmark.classList.remove('hidden');
+
 /* 2- Prueba mostrar menu */
+
+let showMenu = document.querySelector (".menudropdown");
+
+showMenu.classList.remove("collapsed");
+
 /* 3- Añadir informacion de los enlaces desde JS
     3.1 Traer el elemento html
     3.2 Variable que contenga toda la información del li por cada uno de los enlaces
